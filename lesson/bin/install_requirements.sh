9d149613bfc614c578b3863fe405054ec9f13c75
hadramet <hadramet.sylla@outlook.fr>
2023-03-08 10:42:49 +0100

    update fix for requirements.txt

--- lesson/bin/install_requirements.sh
+++ lesson/bin/install_requirements.sh
@@ -4,5 +4,5 @@ echo "Initialization: Installing requirements"
 python -V
 python -m ensurepip --default-pip
 python -m pip install --upgrade pip
-python -m pip freeze > requirements.txt
+tr -d '\r' < requirements.txt > requirements.txt
 python -m pip install -r requirements.txt

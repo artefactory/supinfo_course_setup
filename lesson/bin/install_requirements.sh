9230a7a5e7c86bdb068d863ecc281d29e09c422e
hadramet <hadramet.sylla@outlook.fr>
2023-03-08 04:15:41 +0100

    fix  Unix line endings (\n) when pull on windows

--- lesson/bin/install_requirements.sh
+++ lesson/bin/install_requirements.sh
@@ -3,4 +3,5 @@
 echo "Initialization: Installing requirements"
 python -V
 python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
+python -m pip freeze > requirements.txt
+# python -m pip install -r requirements.txt

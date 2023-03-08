8e4a3da572ed3e1a4cc5e58f8c0ea361cd8c60a3
hadramet <hadramet.sylla@outlook.fr>
2023-03-08 05:08:31 +0100

    add requirement installation

--- lesson/bin/install_requirements.sh
+++ lesson/bin/install_requirements.sh
@@ -2,6 +2,7 @@
 
 echo "Initialization: Installing requirements"
 python -V
+python -m ensurepip --default-pip
 python -m pip install --upgrade pip
 python -m pip freeze > requirements.txt
-# python -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
